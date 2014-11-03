69ddf0a86b63cb8d1112b26a1ddd0948488f148a
Rain Liu <yuliu@outlook.com>
2014-11-02 22:37:39 -0800

    add test

--- graph/Graph_test.go
+++ graph/Graph_test.go
@@ -9,12 +9,12 @@ import (
 
 func TestGraph(t *testing.T) {
 	fd, err := os.Open("tinyG.txt")
-    if err != nil {
-        panic(fmt.Sprintf("open %s: %v", "tinyG.txt", err))
+	if err != nil {
+    	panic(fmt.Sprintf("open %s: %v", "tinyG.txt", err))
     }
-    defer fd.Close()
+	defer fd.Close()
 
-    g := NewGraphFromReader(fd);
+	g := NewGraphFromReader(fd);
 
-    fmt.Printf(g.String())
+	fmt.Printf(g.String())
 }

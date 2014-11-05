b2a3e7a2cbf4aeffdb03dd9ad747ca8a2cc3d49a
Rain Liu <yuliu@outlook.com>
2014-11-04 21:50:23 -0800

    add DegreeOfSeparation test

--- graph/SymbolGraph_test.go
+++ graph/SymbolGraph_test.go
@@ -45,3 +45,56 @@ func CallSymbolGraph(filename, command, delim string) {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 }
+
+func TestDegreeOfSeparation(t *testing.T) {
+	CallDegreeOfSeparation("routes.txt", "routes_cmd.txt", " ", "JFK")
+}
+
+func CallDegreeOfSeparation(filename, command, delim, source string) {
+	fd, err := os.Open(filename)
+	if err != nil {
+		panic(fmt.Sprintf("open %s: %v", filename, err))
+	}
+	defer fd.Close()
+
+	sg := NewSymbolGraph(fd, delim)
+	g := sg.G()
+	//fmt.Printf("%s\n", g.String())
+
+	if !sg.Contains(source) {
+		fmt.Printf("%s not in database\n", source)
+		return
+	}
+
+	s := sg.Index(source)
+	bfs := NewBFS(g, s)
+
+	cmd, err2 := os.Open(command)
+	if err2 != nil {
+		panic(fmt.Sprintf("open %s: %v", command, err2))
+	}
+	defer cmd.Close()
+
+	scanner := bufio.NewScanner(cmd)
+	for scanner.Scan() {
+		sink := scanner.Text()
+		fmt.Printf("%s\n", sink)
+		if sg.Contains(sink) {
+			t := sg.Index(sink)
+			if bfs.HasPathTo(t) {
+				iter := bfs.PathTo(t).Iterator()
+				for iter.HasNext() {
+					v := iter.Next().Value.(int)
+					fmt.Printf("   %s\n", sg.Name(v))
+				}
+			} else {
+				fmt.Printf("Not connected\n")
+			}
+		} else {
+			fmt.Printf("Not in database\n")
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+}

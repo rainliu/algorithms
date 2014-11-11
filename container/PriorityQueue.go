5272816cad232ad03106fb8391ebfab18017d7c9
Rain Liu <yuliu@outlook.com>
2014-11-10 21:51:07 -0800

    refactoring QP to support max/minPQ

--- container/PriorityQueue.go
+++ container/PriorityQueue.go
@@ -1,66 +1,76 @@
 package container
 
 import (
-	"algorithms"	
+	"algorithms"
 )
 
-type PriorityQueueItem struct{
-	Index   int
-	Key 	algorithms.Comparable
+type PriorityQueueItem struct {
+	Index int
+	Key   algorithms.Comparable
 }
 
-type PriorityQueue struct{		
+type priority_queue struct {
+	max_pq  bool
 	current int
-	size	int
-	pq		[]int
+	size    int
+	pq      []int
 	qp      []int
 	keys    []algorithms.Comparable
 }
 
-func NewPriorityQueue(size int) *PriorityQueue{
-	this := &PriorityQueue{}
+func NewMaxPriorityQueue(size int) *priority_queue {
+	return NewPriorityQueue(size, true)
+}
+
+func NewMinPriorityQueue(size int) *priority_queue {
+	return NewPriorityQueue(size, false)
+}
+
+func NewPriorityQueue(size int, max_pq bool) *priority_queue {
+	this := &priority_queue{}
 
 	this.pq = make([]int, size+1)
 	this.qp = make([]int, size+1)
 	this.keys = make([]algorithms.Comparable, size+1)
 	this.size = 0
 	this.current = 0
+	this.max_pq = max_pq
 
-	for i:=0; i<=size; i++{
+	for i := 0; i <= size; i++ {
 		this.qp[i] = -1
 	}
 
-	return this;
+	return this
 }
 
-func (this *PriorityQueue) IsEmpty() bool{
-	return this.size==0
+func (this *priority_queue) IsEmpty() bool {
+	return this.size == 0
 }
 
-func (this *PriorityQueue) Size() int{
+func (this *priority_queue) Size() int {
 	return this.size
 }
 
-func (this *PriorityQueue) Push(v interface{}){	
-	if pqi, ok := v.(*PriorityQueueItem); ok{
-		if this.Contains(pqi.Index){
+func (this *priority_queue) Push(v interface{}) {
+	if pqi, ok := v.(*PriorityQueueItem); ok {
+		if this.Contains(pqi.Index) {
 			panic("index is already in the priority queue\n")
-		}else{
-			this.size++			
-			
+		} else {
+			this.size++
+
 			this.qp[pqi.Index] = this.size
 			this.pq[this.size] = pqi.Index
 			this.keys[pqi.Index] = pqi.Key
 
 			this.swim(this.size)
 		}
-	}else{
-		panic("input v is not *PriorityQueueItem\n")		
+	} else {
+		panic("input v is not *PriorityQueueItem\n")
 	}
 }
 
-func (this *PriorityQueue) Pop() *Item{
-	if this.size==0{
+func (this *priority_queue) Pop() *Item {
+	if this.size == 0 {
 		panic("Priority queue underflow\n")
 	}
 
@@ -68,8 +78,8 @@ func (this *PriorityQueue) Pop() *Item{
 
 	item := &Item{}
 	item.Next = nil
-	item.Value = &PriorityQueueItem{this.pq[1], this.keys[this.pq[1]]} ;
-	
+	item.Value = &PriorityQueueItem{this.pq[1], this.keys[this.pq[1]]}
+
 	this.swap(1, this.size)
 	this.size--
 	this.sink(1)
@@ -77,18 +87,16 @@ func (this *PriorityQueue) Pop() *Item{
 	this.qp[idx] = -1
 	this.keys[this.pq[this.size+1]] = nil
 	this.pq[this.size+1] = -1
-	
-		
-	return item;
+
+	return item
 }
 
-
-func (this *PriorityQueue) Contains(i int) bool{
-	return this.qp[i]!=-1
+func (this *priority_queue) Contains(i int) bool {
+	return this.qp[i] != -1
 }
 
-func (this *PriorityQueue) ChangeKey(i int, key algorithms.Comparable) {
-	if !this.Contains(i){
+func (this *priority_queue) ChangeKey(i int, key algorithms.Comparable) {
+	if !this.Contains(i) {
 		panic("index is not in the priority queue\n")
 	}
 	this.keys[i] = key
@@ -96,41 +104,40 @@ func (this *PriorityQueue) ChangeKey(i int, key algorithms.Comparable) {
 	this.sink(this.qp[i])
 }
 
-
-func (this *PriorityQueue) IncreaseKey(i int, key algorithms.Comparable) {
-	if !this.Contains(i){
+func (this *priority_queue) IncreaseKey(i int, key algorithms.Comparable) {
+	if !this.Contains(i) {
 		panic("index is not in the priority queue\n")
 	}
 	if this.keys[i].CompareTo(key) >= 0 {
-		panic("Calling IncreaseKey() with given argument would not strictly increase the key\n");
+		panic("Calling IncreaseKey() with given argument would not strictly increase the key\n")
 	}
 
 	this.keys[i] = key
 	this.swim(this.qp[i])
 }
 
-func (this *PriorityQueue) DecreaseKey(i int, key algorithms.Comparable) {
-	if !this.Contains(i){
+func (this *priority_queue) DecreaseKey(i int, key algorithms.Comparable) {
+	if !this.Contains(i) {
 		panic("index is not in the priority queue\n")
 	}
 	if this.keys[i].CompareTo(key) <= 0 {
-		panic("Calling DecreaseKey() with given argument would not strictly decrease the key\n");
+		panic("Calling DecreaseKey() with given argument would not strictly decrease the key\n")
 	}
 
 	this.keys[i] = key
 	this.sink(this.qp[i])
 }
 
-func (this *PriorityQueue) Iterator() Iterator {
+func (this *priority_queue) Iterator() Iterator {
 	this.current = 1
 	return this
 }
 
-func (this *PriorityQueue) HasNext() bool {
+func (this *priority_queue) HasNext() bool {
 	return this.current != this.size+1
 }
 
-func (this *PriorityQueue) Next() *Item {
+func (this *priority_queue) Next() *Item {
 	item := &Item{}
 	item.Next = nil
 	item.Value = &PriorityQueueItem{this.pq[this.current], this.keys[this.pq[this.current]]}
@@ -138,12 +145,16 @@ func (this *PriorityQueue) Next() *Item {
 	return item
 }
 
-func (this *PriorityQueue) less(i, j int) bool{
-	return this.keys[this.pq[i]].CompareTo(this.keys[this.pq[j]]) < 0
+func (this *priority_queue) comp(i, j int) bool {
+	if this.max_pq {
+		return this.keys[this.pq[i]].CompareTo(this.keys[this.pq[j]]) < 0
+	} else {
+		return this.keys[this.pq[i]].CompareTo(this.keys[this.pq[j]]) > 0
+	}
 }
 
-func (this *PriorityQueue) swap(i, j int){
-	t:=this.pq[i]
+func (this *priority_queue) swap(i, j int) {
+	t := this.pq[i]
 	this.pq[i] = this.pq[j]
 	this.pq[j] = t
 
@@ -151,23 +162,23 @@ func (this *PriorityQueue) swap(i, j int){
 	this.qp[this.pq[j]] = j
 }
 
-func (this *PriorityQueue) swim(k int){
-	for k>1 && this.less(k/2, k) {
+func (this *priority_queue) swim(k int) {
+	for k > 1 && this.comp(k/2, k) {
 		this.swap(k/2, k)
-		k=k/2
+		k = k / 2
 	}
 }
 
-func (this *PriorityQueue) sink(k int){
+func (this *priority_queue) sink(k int) {
 	for 2*k <= this.size {
-		j:=2*k
-		if j<this.size && this.less(j, j+1) {
-			j++;
+		j := 2 * k
+		if j < this.size && this.comp(j, j+1) {
+			j++
 		}
-		if !this.less(k, j) {
-			break;
+		if !this.comp(k, j) {
+			break
 		}
-		this.swap(k,j)
-		k=j
+		this.swap(k, j)
+		k = j
 	}
 }
--- container/PriorityQueue_test.go
+++ container/PriorityQueue_test.go
@@ -2,61 +2,60 @@ package container
 
 import (
 	"algorithms"
-    "testing"
-    "fmt"
+	"fmt"
+	"testing"
 )
 
-func TestPriorityQueue(t *testing.T) {	
+func TestPriorityQueue(t *testing.T) {
 	// insert a bunch of strings
-    var strs = []string{ "it", "was", "the", "best", "of", "times", "it", "was", "the", "worst" };
+	var strs = []string{"it", "was", "the", "best", "of", "times", "it", "was", "the", "worst"}
 
-    pq := NewPriorityQueue(len(strs));
-    for i := 0; i < len(strs); i++ {
-        pq.Push(&PriorityQueueItem{i, algorithms.String(strs[i])});
-    }
+	pq := NewMinPriorityQueue(len(strs))
+	for i := 0; i < len(strs); i++ {
+		pq.Push(&PriorityQueueItem{i, algorithms.String(strs[i])})
+	}
 
-    // print each key using the iterator
-    iter := pq.Iterator()    
-    for iter.HasNext() {
-    	i := iter.Next().Value.(*PriorityQueueItem).Index
-        fmt.Printf("%d %s ", i, strs[i]);
-    }
-    fmt.Printf("\n");
+	// print each key using the iterator
+	iter := pq.Iterator()
+	for iter.HasNext() {
+		i := iter.Next().Value.(*PriorityQueueItem).Index
+		fmt.Printf("%d %s ", i, strs[i])
+	}
+	fmt.Printf("\n")
 
-    // increase or decrease the key
-    for i := 0; i < len(strs); i++ {
-        if i%2==0{
-            pq.IncreaseKey(i, algorithms.String(strs[i] + strs[i]));
-        }else{
-            pq.DecreaseKey(i, algorithms.String(strs[i][0:1]));
-        }
-    }
+	// increase or decrease the key
+	for i := 0; i < len(strs); i++ {
+		if i%2 == 0 {
+			pq.IncreaseKey(i, algorithms.String(strs[i]+strs[i]))
+		} else {
+			pq.DecreaseKey(i, algorithms.String(strs[i][0:1]))
+		}
+	}
 
-    // delete and print each key
-    for !pq.IsEmpty() {
-    	pqi := pq.Pop().Value.(*PriorityQueueItem)
-        key := pqi.Key;
-        i   := pqi.Index;
-        fmt.Printf("%d %s ", i, key);
-    }
-    fmt.Printf("\n");
+	// delete and print each key
+	for !pq.IsEmpty() {
+		pqi := pq.Pop().Value.(*PriorityQueueItem)
+		key := pqi.Key
+		i := pqi.Index
+		fmt.Printf("%d %s ", i, key)
+	}
+	fmt.Printf("\n")
 
-/*
-    // reinsert the same strings
-    for (int i = 0; i < strings.length; i++) {
-        pq.insert(i, strings[i]);
-    }
+	/*
+	   // reinsert the same strings
+	   for (int i = 0; i < strings.length; i++) {
+	       pq.insert(i, strings[i]);
+	   }
 
-    // delete them in random order
-    int[] perm = new int[strings.length];
-    for (int i = 0; i < strings.length; i++)
-        perm[i] = i;
-    StdRandom.shuffle(perm);
-    for (int i = 0; i < perm.length; i++) {
-        String key = pq.keyOf(perm[i]);
-        pq.delete(perm[i]);
-        StdOut.println(perm[i] + " " + key);
-    }
- */
+	   // delete them in random order
+	   int[] perm = new int[strings.length];
+	   for (int i = 0; i < strings.length; i++)
+	       perm[i] = i;
+	   StdRandom.shuffle(perm);
+	   for (int i = 0; i < perm.length; i++) {
+	       String key = pq.keyOf(perm[i]);
+	       pq.delete(perm[i]);
+	       StdOut.println(perm[i] + " " + key);
+	   }
+	*/
 }
-
